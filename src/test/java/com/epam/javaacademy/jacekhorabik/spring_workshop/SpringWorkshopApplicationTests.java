27de9bd1dcd0973e53005ae59e3b59ab1e59ed27
jacek_horabik <jacek_horabik@epam.com>
2021-01-27 11:03:52 +0100

    Migawka początkowa.

---
+++ src/test/java/com/epam/javaacademy/jacekhorabik/spring_workshop/SpringWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.javaacademy.jacekhorabik.spring_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
